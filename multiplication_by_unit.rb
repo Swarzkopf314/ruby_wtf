1ee00bf8d7ed16260b04950c0f8b591d9df841d6
Maciej Satkiewicz <msatkiewicz@gmail.com>
2017-03-19 19:41:19 +0100

    comment

--- multiplication_by_unit.rb
+++ multiplication_by_unit.rb
@@ -1,11 +1,25 @@
 # checked for ruby-version 2.4.0
 
+####
+# This script shows that ruby conversion of Float into Decimal is somehow inconsistent;
+# One would reasonably expect the equality 1.to_d * f == f.t_d to hold for every Float value f,
+# i.e. multiplying by DigDecimal unit can be viewed as alternative way of casting Float into Decimal.
+# Yet for some Floats (e.g. 64.4) the results differ, as showed in this script.
+
+# My original question on stackoverflow:
+# http://stackoverflow.com/questions/40472933/inconsistent-conversion-of-float-into-decimal-in-ruby/40473007
+
+# As Stefan points out in his exhaustive answer, it is due to different precisions being used:
+# 64.4.to_d is equivalent to BigDecimal(64.4, Float::DIG)
+# while
+# 1.to_d * 64.4 translates to BigDecimal(64.4, Float::DIG + 1)
+
 require 'bigdecimal'
 require 'bigdecimal/util' # add method to_d
 
-GLOBAL = binding
+GLOBAL = binding # scoping trick
 
-# eval and print
+# eval the code and print the code and it's result
 def ep(code, check = nil, comment = nil)
   evaled = GLOBAL.eval(code)
 
@@ -42,4 +56,9 @@ ep "f * 1.to_d == 1.to_d * f", true
 
 ep "f.to_d" # => #<BigDecimal:7f8202038280,'0.644E2',18(36)>
 ep "1.to_d * f" # => #<BigDecimal:7f82019c1208,'0.6440000000 000001E2',27(45)>
+
+# precision check:
+# http://stackoverflow.com/questions/40472933/inconsistent-conversion-of-float-into-decimal-in-ruby/40473007
+ep "f.to_d(16) == 1.to_d * f", true
+ep "f.to_d == 1.to_d.mult(f, 15)", true
 
